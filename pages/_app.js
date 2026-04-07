ec0e77529c931d1e5f9e89a17ea2e699a65dfd13
Harrison <harrisonmcgarry144@gmail.com>
2026-04-07 21:35:38 +0100

    Fix viewport zoom

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,25 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import Layout from "../components/Layout";
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
-return (
-  <AuthProvider>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    <Analytics />
-    <SpeedInsights />
-  </AuthProvider>
-);
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <title>EduPositive</title>
+      </Head>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
